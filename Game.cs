e59eb3a3a38c24f4785c700e7d26ccf0999632ed
Benjamin Brownlie <benjibspam@gmail.com>
2020-09-19 19:31:24 -0500

    Started work on player function

--- Game.cs
+++ Game.cs
@@ -6,16 +6,35 @@ namespace HelloWorld
 {
     class Game
     {
+        Player player;
+        bool gameOver = false;
+
+        public Player CreateCharacter()
+        {
+            Console.WriteLine("Welcome to your first shift at the Latenight convenience store. Can I get your name please?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + "! Please head into the office and clock in.");
+            Player player = new Player(name, 100, 5, 0);
+            return player;
+        }
         //Run the game
         public void Run()
         {
+            Start();
+
+            while (gameOver == false)
+            {
+                Update();
+            }
+
+            End();
             
         }
 
         //Performed once when the game begins
         public void Start()
         {
-            
+            CreateCharacter();
         }
 
         //Repeated until the game ends
---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Player
+    {
+        public string playerName;
+        public int playerHealth;
+        public int playerDamage;
+        public int playerFear;
+
+        public Player()
+        {
+            playerHealth = 100;
+            playerDamage = 5;
+        }
+
+        public Player(string nameVal, int healthVal, int damageVal, int fearVal)
+        {
+            playerName = nameVal;
+            playerHealth = healthVal;
+            playerDamage = damageVal;
+            playerFear = fearVal;
+        }
+    }
+}
